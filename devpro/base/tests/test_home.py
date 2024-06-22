7b064f66e262da8a07dfcd7be6d0bf55fe8452b2
diego <ruan.diego.anjos@outlook.com>
2024-06-21 22:26:31 -0300

    instalando a app base

---
+++ devpro/base/tests/test_home.py
@@ -0,0 +1,18 @@
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+from devpro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def response(client: Client):
+    return client.get(reverse('base:home'))
+
+
+def test_home_status_code(response):
+    assert response.status_code == 200
+
+
+def test_titulo_home(response):
+    assert_contains(response, '<title>home')
